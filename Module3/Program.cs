f774e7927ff6d3b274f09a585fad2f0f3eae1c92
SickleShaped <SickleShaped@gmail.com>
2024-03-06 23:20:36 +0300

    Добавьте файлы проекта.

---
+++ Module3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Module3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Sunny";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
