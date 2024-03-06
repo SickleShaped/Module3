6b8ddf719c615a3c7ee5e2885bce1a5c40a98308
SickleShaped <SickleShaped@gmail.com>
2024-03-06 23:27:56 +0300

    Added x23 writeline

--- Module3/Program.cs
+++ Module3/Program.cs
@@ -1,14 +1,21 @@
 ﻿using System;
+using System.Xml.Linq;
+
 namespace Module3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string myName;
-            myName = "Sunny";
+            string myName = "Sunny";
 
             Console.WriteLine(myName);
+
+            Console.WriteLine("\t Привет Мир");
+            Console.WriteLine("\t Мне 27 лет");
+            Console.WriteLine($"\t My name is \n {myName}");
+            Console.WriteLine("\u0040");
+            Console.WriteLine('\x23');
 
             Console.ReadKey();
         }
