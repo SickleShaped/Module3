6e65121ea377021b64887d4e4c9ba55819921762
SickleShaped <SickleShaped@gmail.com>
2024-03-07 08:22:54 +0300

    added Semaphore Enum

---
+++ Module3/Enum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module3
+{
+    enum Semaphore
+    {
+        Red=100,
+        Yellow=200,
+        Green=300,
+    }
+
+}
